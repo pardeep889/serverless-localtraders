0e7d1c4dc1f9ecd58dfbbc18da61855216d39505
Rupinderkaushal <rupinder.xpertiks@gmail.com>
2023-02-02 17:58:33 +0530

    add trading database and utils

---
+++ trading-service/utils.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const send = (statusCode, data) => {
+  const responseHeaders = {
+    'Content-Type': 'application/json',
+    // Required for CORS support to work
+    'Access-Control-Allow-Origin': '*',  
+    // Required for cookies, authorization headers with HTTPS
+    'Access-Control-Allow-Credentials': true
+  };
+  return {
+      statusCode: statusCode,
+      headers: responseHeaders,
+      body: JSON.stringify(
+        data,
+        null,
+        2
+      ),
+    };
+};
+
+module.exports = {
+    send
+}
