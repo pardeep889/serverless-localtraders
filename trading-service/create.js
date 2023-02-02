8e766a1c492ffb8d3dcc9d0d75f445a9e92a14b7
Rupinderkaushal <rupinder.xpertiks@gmail.com>
2023-02-02 17:57:48 +0530

    create trading service endpoint

---
+++ trading-service/create.js
@@ -0,0 +1,54 @@
+const AWS = require("aws-sdk");
+const utils = require("./utils");
+
+
+const dynamoDbClient = new AWS.DynamoDB.DocumentClient();
+
+module.exports.handler = async (event) => {
+  try {
+    const offerdata = JSON.parse(event.body);
+    let {username,paymentMethodCategory,paymentMethod,avgTime,price,currency,type,location } = offerdata;
+
+    const id = Date.now() + Math.random().toString(36).substring(2, 15);
+    const createdAt = Date.now()
+
+
+    if (!offerdata) {
+      return utils.send(400, {
+        message: event.body,
+        data: {},
+      });
+    }
+
+    // add user to db
+    const params = {
+      TableName: "trading-offer",
+      Item: {
+        id,
+        username,
+        paymentMethodCategory,
+        paymentMethod,
+        price,
+        currency,
+        location,
+        avgTime,
+        type,
+        createdAt
+      },
+    };
+
+    const resp = await dynamoDbClient.put(params).promise();
+    return utils.send(200, {
+      data: {
+        isAuth: true,
+        data : resp,
+      },
+    });
+  } catch (error) {
+ 
+    return utils.send(400, {
+      message: "something went wrong",
+      data: {},
+    });
+  }
+};
