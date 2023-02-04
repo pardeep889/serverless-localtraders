8a7a633367db59406a49a372cacb3d91e0d36382
pardeep-inbev <pardeep889@hotmail.com>
2023-02-04 09:40:39 +0530

    token response headers

--- admin-service/token.js
+++ admin-service/token.js
@@ -36,9 +36,12 @@ const verifyUser = async (req) => {
 
 const send = (statusCode, data) => {
   const responseHeaders = {
-    "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Credentials": true,
+    'Content-Type': 'application/json',
+    // Required for CORS support to work
+    'Access-Control-Allow-Origin': '*',  
+    // Required for cookies, authorization headers with HTTPS
+    'Access-Control-Allow-Credentials': true,
+    'Access-Control-Allow-Methods': '*'
   };
 
   return {
