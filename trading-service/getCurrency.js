f0e81f7162c34a76bf93ea8c8bd49df1663761cd
Rupinderkaushal <rupinder.xpertiks@gmail.com>
2023-02-06 14:49:03 +0530

    create endpoint for curencies

---
+++ trading-service/getCurrency.js
@@ -0,0 +1,22 @@
+const utils = require("./utils");
+const AWS = require("aws-sdk");
+const { default: axios } = require("axios");
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.handler = async (request) => {
+
+  try {
+
+    const API_KEY = "6a2b84bd-59b5-409b-83ec-2dd38d982e4d"
+    const URL = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest';
+    
+   const currencies = await axios.get(URL, {
+      headers: {
+        'X-CMC_PRO_API_KEY': API_KEY
+      }
+    })
+    return utils.send(200, { message: "currencies retrieved successfully", data:currencies?.data.data });
+  } catch (error) {
+    return utils.send(400, { message: "Unable to retrieve currencies", error });
+  }
+};
