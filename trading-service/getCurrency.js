67678833fdf7a6d12658dfe760db676a6695566a
Rupinderkaushal <rupinder.xpertiks@gmail.com>
2023-02-08 14:01:39 +0530

    fix in getCurrency in trading service

--- trading-service/getCurrency.js
+++ trading-service/getCurrency.js
@@ -5,19 +5,11 @@ const { default: axios } = require("axios");
 module.exports.handler = async (request) => {
 
   try {
-
-    const API_KEY = "6a2b84bd-59b5-409b-83ec-2dd38d982e4d"
-    const URL = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest';
-    
-   const currencies = await axios.get(URL, {
-      headers: {
-        'X-CMC_PRO_API_KEY': API_KEY
-      }
-    })
-    
-    const targetSymbols = new Set(['lct', 'bnb', 'btc', 'eth','usdt','usdc','busd'].map((val) => val.toLowerCase()));
-    const result = currencies?.data?.data?.reduce((filtered, filterValue) => {
-      const { name, symbol } = filterValue;
+    const URL='https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true&price_change_percentage=7d'
+    const currencies = await axios.get(URL)
+    const targetSymbols = new Set(['bnb', 'btc', 'eth','usdt','usdc','busd'].map((val) => val.toLowerCase()));
+    const result = currencies?.data?.reduce((filtered, filterValue) => {
+      const {symbol } = filterValue;
       if (targetSymbols.has(symbol.toLowerCase())) {
         filtered.push(filterValue);
       }
