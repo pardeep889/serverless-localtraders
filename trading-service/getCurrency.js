da63d02822cd7fb6a9205cd137e3d3583ade5c27
Rupinderkaushal <rupinder.xpertiks@gmail.com>
2023-02-09 18:30:30 +0530

    add new api for lcdt

--- trading-service/getCurrency.js
+++ trading-service/getCurrency.js
@@ -16,7 +16,33 @@ module.exports.handler = async (request) => {
       return filtered;
     }, []);
 
-    return utils.send(200, { message: "currencies retrieved successfully", data:result });
+    // second api
+
+    const LCDTURL = "https://widget.nomics.com/api/assets/LCT3/BUSD/"
+    const lcdResponse = await axios.post(LCDTURL)
+    const newResp = lcdResponse?.data
+
+    const restructureResp = {
+      "id" : newResp?.meta?.id,
+      "symbol": newResp?.meta?.symbol,
+      "name": newResp?.meta?.name,
+      "image": newResp?.meta?.logo_url,
+      "current_price": newResp?.meta?.price,
+      "market_cap": newResp?.meta?.market_cap_dominance,
+      "market_cap_rank": newResp?.meta?.rank,
+      "high_24h": newResp?.meta?.high,
+      "price_change_24h": newResp?.day.price_change,
+      "price_change_percentage_24h": newResp?.day.price_change_pct,
+      "market_cap_change_24h": newResp?.day.volume_change,
+      "market_cap_change_percentage_24h": newResp?.day.volume_change_pct,
+      "ath_date": newResp?.meta?.price_timestamp,
+      "last_updated": newResp?.meta?.price_timestamp,
+      "sparkline_in_7d": {
+        "price" : newResp?.history
+      }
+    }
+
+    return utils.send(200, { message: "currencies retrieved successfully", data:[...result,restructureResp] });
   } catch (error) {
     return utils.send(400, { message: "Unable to retrieve currencies", error });
   }
