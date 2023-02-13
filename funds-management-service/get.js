fa1f6ca29f792a665ad2062454ab0cc7d00bb46c
Rupinderkaushal <rupinder.xpertiks@gmail.com>
2023-02-13 17:58:51 +0530

    get funds by id

---
+++ funds-management-service/get.js
@@ -0,0 +1,28 @@
+const utils = require("./utils");
+const AWS = require("aws-sdk");
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.handler = async (request) => {
+  const { id } = request.pathParameters;
+  if (!id) {
+    return utils.send(400, {
+      message: "Missing id path param",
+      data: {},
+    });
+  }
+
+  const params = {
+    TableName: "Add-Funds",
+    Key: {id},
+  };
+
+  try {
+    const data = await dynamoDb.get(params).promise();
+    console.log({data})
+
+    return utils.send(200, { message: "Fund retrieved successfully", data:data?.Item || {}});
+  } catch (error) {
+    return utils.send(400, { message: "Unable to retrieve fund", error });
+  }
+};
