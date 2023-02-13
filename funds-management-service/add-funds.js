861830f18364b7d96254fcb7009249058a81f49f
Rupinderkaushal <rupinder.xpertiks@gmail.com>
2023-02-13 17:42:10 +0530

    add funds and list funds

---
+++ funds-management-service/add-funds.js
@@ -0,0 +1,123 @@
+const utils = require("./utils");
+const AWS = require("aws-sdk");
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+const getUser = async (id) => {
+  const params = {
+    TableName: "User",
+    FilterExpression: "id = :id",
+    ExpressionAttributeValues: {
+      ":id": id,
+    },
+  };
+  const result = await dynamoDb.scan(params).promise();
+  return result.Items[0];
+};
+
+const checkIfAssetExitsForSymbol = async (symbol, userId) => {
+  console.log({ symbol, userId });
+  // check if user exist in our db
+  const paramsScan = {
+    TableName: "Asset",
+    FilterExpression: "symbol = :symbol and userId = :userId",
+    ExpressionAttributeValues: {
+      ":symbol": symbol,
+      ":userId": userId,
+    },
+  };
+
+  const data = await dynamoDb.scan(paramsScan).promise();
+  console.log({ data });
+  return data.Items[0];
+};
+
+module.exports.handler = async (request) => {
+  try {
+    if (!request.body) {
+      return utils.send(400, {
+        message: "request body is missing!",
+      });
+    }
+
+    let funds;
+    try {
+      funds = JSON.parse(request.body);
+    } catch (err) {
+      return utils.send(400, {
+        message: "request for add funds is not a valid JSON",
+      });
+    }
+
+    if (!funds.userId || !funds.symbol || !funds.amount) {
+      return utils.send(400, {
+        message: "missing userId, symbol or amount the body",
+      });
+    }
+
+    let { symbol, amount, userId } = funds;
+    const symbol_lowerCase = symbol.toLowerCase();
+
+    // check if user exits
+    const user = await getUser(userId);
+    console.log("### user ####", user);
+    if (!user) {
+      return utils.send(404, {
+        message: "user with this userId does not exist",
+      });
+    }
+
+    // check if symbol exits for this user
+    const asset = await checkIfAssetExitsForSymbol(symbol_lowerCase, userId);
+    console.log("#### asset ###", asset);
+
+    if (!asset) {
+      return utils.send(404, {
+        message: "user with this symbol does not exist",
+      });
+    }
+    //   update balance for request symbol
+
+    await dynamoDb
+      .update({
+        TableName: "Asset",
+        Key: { assetId: asset?.assetId },
+        UpdateExpression: "set #balance = :balance",
+        ExpressionAttributeValues: {
+          ":balance": amount,
+        },
+        ExpressionAttributeNames: {
+          "#balance": "balance",
+        },
+      })
+      .promise();
+
+    // add entry to funds table
+
+    const id = Date.now() + Math.random().toString(36).substring(2, 15);
+    const added_on = "" + Date.now();
+
+    const fundsData = {
+      id,
+      to: userId,
+      amount,
+      symbol: symbol_lowerCase,
+      added_on,
+    };
+
+    const params = {
+      TableName: "Add-Funds",
+      Item: fundsData,
+    };
+
+    await dynamoDb.put(params).promise();
+    return utils.send(200, {
+      message: "funds add successfuly",
+    });
+  } catch (error) {
+    return utils.send(400, {
+      message: "Unable to add funds. something went wrong",
+      error,
+    });
+  }
+};
---
+++ funds-management-service/utils.js
@@ -0,0 +1,135 @@
+// const jwt = require("jsonwebtoken");
+// const SECRET_KEY = "MyAwesomeKey";
+
+const AWS = require("aws-sdk");
+const dynamoDbClient = new AWS.DynamoDB.DocumentClient();
+
+
+// const getToken = async (user, expiry) => {
+//   return jwt.sign(user, SECRET_KEY, {
+//     expiresIn: expiry,
+//   });
+// };
+// const verifyUser = async (req) => {
+//   //   const SECRET_KEY = "MyAwesomeKey";
+//   let token = req.headers["Authorization"];
+//   if (token) {
+//     jwt.verify(token, SECRET_KEY, function (err, decoded) {
+//       if (err) {
+//         console.log(err);
+//       } else {
+//         req.decoded = decoded;
+//         console.log("all good");
+//       }
+//     });
+//   } else {
+//     console.log("mmmm");
+//   }
+// };
+
+const assets = [
+  { symbol: "btc", assetName: "Bitcoin", balance: 0 },
+  { symbol: "eth", assetName: "Ethereum", balance: 0 },
+  { symbol: "bnb", assetName: "Binance Coin", balance: 0 },
+  { symbol: "tet", assetName: "Tether", balance: 0 },
+  { symbol: "lct", assetName: "LOCAL TRADERS", balance: 0 },
+  { symbol: "usdc", assetName: "USD COIN", balance: 0 },
+  { symbol: "busd", assetName: "Binance USD", balance: 0 },
+];
+
+
+
+
+const send = (statusCode, data) => {
+  const responseHeaders = {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Credentials": true,
+    'Access-Control-Allow-Methods': '*'
+  };
+
+  return {
+    statusCode,
+    headers: responseHeaders,
+    body: JSON.stringify(data, null, 2),
+  };
+};
+
+
+const initalizeWallet = async (userId) => {
+  try {
+    // check if this userId already have the wallet
+    const walletParams = {
+      TableName: "Wallet",
+      FilterExpression: "userId = :userId",
+      ExpressionAttributeValues: {
+        ":userId": userId,
+      },
+    };
+
+    const isWalletAlreadyExists = await dynamoDbClient
+      .scan(walletParams)
+      .promise();
+    if (isWalletAlreadyExists.Items.length) {
+      throw new Error("user already have the wallet");
+    }
+
+    // create wallet for userId
+
+    const walletId =
+      "wid-" + Date.now() + Math.random().toString(36).substring(2, 15);
+    const walletData = {
+      walletId: walletId,
+      userId,
+      isWalletActive: false,
+    };
+
+    const params = {
+      TableName: "Wallet",
+      Item: walletData,
+    };
+
+    const walletResponse = await dynamoDbClient.put(params).promise();
+    return walletResponse;
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+
+async function createAssetsForUser(userId) {
+  try {
+        // Create assets for userId
+        const promises = assets.map((asset) => {
+          const id = Date.now() + Math.random().toString(36).substring(2, 15);
+          const assetData = {
+            assetId: id,
+            userId,
+            balance: asset.balance,
+            assetName: asset.assetName,
+            symbol: asset.symbol,
+            address: "",
+          };
+    
+          const params = {
+            TableName: "Asset",
+            Item: assetData,
+          };
+    
+          return dynamoDbClient.put(params).promise();
+        });
+    
+        await Promise.all(promises);
+    
+
+  } catch (err) {
+    throw new Error(err);
+  }
+}
+
+module.exports = {
+  // getToken,
+  // verifyUser,
+  send,
+  initalizeWallet,
+  createAssetsForUser,
+};
