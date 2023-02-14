c2729e82fa501cc4c0f225fb8bc8ffb800131043
Rupinderkaushal <rupinder.xpertiks@gmail.com>
2023-02-14 18:40:11 +0530

    fix some issue

--- funds-management-service/transfer-funds.js
+++ funds-management-service/transfer-funds.js
@@ -28,33 +28,28 @@ async function retrieveAssetBalance(id, symbol) {
 
 // Update sender's balance in database
 async function updateBalance(assetId, updatedBalance) {
-    try {
-        
-        const paramsUpdate = {
-            TableName: "Asset",
-            Key: { assetId:assetId },
-            UpdateExpression: "set #balance = :balance",
-            ExpressionAttributeValues: {
-              ":balance": updatedBalance,
-            },
-            ExpressionAttributeNames: {
-              "#balance": "balance",
-            },
-          }
-      const updateResp =  await dynamoDb.update(paramsUpdate).promise();
-      console.log({updateResp})
-    } catch (error) {
-        console.log("upated error",{ error });
-        throw new Error(error);
-    }
+  try {
+    const paramsUpdate = {
+      TableName: "Asset",
+      Key: { assetId: assetId },
+      UpdateExpression: "set #balance = :balance",
+      ExpressionAttributeValues: {
+        ":balance": updatedBalance,
+      },
+      ExpressionAttributeNames: {
+        "#balance": "balance",
+      },
+    };
+    await dynamoDb.update(paramsUpdate).promise();
+  } catch (error) {
+    console.log("upated error", { error });
+    throw new Error(error);
   }
-async function retrieveRecipientBalance(recipient_id) {
+}
+async function createTransaction(recipient_id) {
   // Code to retrieve recipient's balance from database goes here
 }
 
-async function updateRecipientBalance(recipient_id, updatedRecipientBalance) {
-  // Code to update recipient's balance in database goes here
-}
 
 
 module.exports.handler = async (request) => {
@@ -88,47 +83,41 @@ module.exports.handler = async (request) => {
 
     // check if user exits
 
-    const senderResp = await retrieveAssetBalance(
-      sender_id,
-      symbol
-    );
-    console.log("sender rr##### ",{ senderResp });
-    
-    if(!senderResp){
-        return utils.send(400, {
-            message: `sender user doesn't exists or  doesn't have ${symbol} symbol`,
-          });
+    const senderResp = await retrieveAssetBalance(sender_id, symbol);
+    console.log("sender rr##### ", { senderResp });
+
+    if (!senderResp) {
+      return utils.send(400, {
+        message: `sender user doesn't exists or  doesn't have ${symbol} symbol`,
+      });
     }
 
-    if(parseFloat(senderResp?.balance) < amount) {
+    if (parseFloat(senderResp?.balance) < amount) {
       return utils.send(400, {
         message: "sender doesn't have sufficient balance to transfer",
       });
     }
 
     const updated_balance_of_sender = parseFloat(senderResp?.balance) - amount;
-    console.log({updated_balance_of_sender})
+    console.log({ updated_balance_of_sender });
 
     // get the balance of receipient for required symbol
-    const receipientResp = await retrieveAssetBalance(
-        recipient_id,
-        symbol
-      );
-      console.log("receipent rr##### ",{ receipientResp });
-      
-      if(!receipientResp){
-          return utils.send(400, {
-              message: `receiver user doesn't exists or  doesn't have ${symbol} symbol`,
-            });
-      }
-  
-      const updated_balance_of_receiver = parseFloat(receipientResp?.balance) + amount;
-      console.log({updated_balance_of_receiver})
+    const receipientResp = await retrieveAssetBalance(recipient_id, symbol);
+    console.log("receipent rr##### ", { receipientResp });
 
-      // update balance for sender and receiver
-      updateBalance(senderResp.assetId,updated_balance_of_sender)
-      updateBalance(receipientResp.assetId,updated_balance_of_receiver)
+    if (!receipientResp) {
+      return utils.send(400, {
+        message: `receiver user doesn't exists or  doesn't have ${symbol} symbol`,
+      });
+    }
 
+    const updated_balance_of_receiver =
+      parseFloat(receipientResp?.balance) + amount;
+    console.log({ updated_balance_of_receiver });
+
+    // update balance for sender and receiver
+    await updateBalance(senderResp.assetId, updated_balance_of_sender);
+    await updateBalance(receipientResp.assetId, updated_balance_of_receiver);
 
     //   update balance for request symbo
     return utils.send(200, {
